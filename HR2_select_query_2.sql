412984a7630a49c830e3a64853607f310de4a09b
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-11-19 06:50:05 +0530

    select and count

--- HR2_select_query_2.sql
+++ HR2_select_query_2.sql
@@ -19,3 +19,8 @@ where ID = 1661;
 -- Solution :
 select * from CITY
 where COUNTRYCODE = 'JPN';
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+-- Solution :
+select NAME from city
+where COUNTRYCODE = 'JPN';
