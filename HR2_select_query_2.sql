52d5aec2a655bff503040382f196e4f7c09ec52c
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-11-22 06:40:57 +0530

    union all

--- HR2_select_query_2.sql
+++ HR2_select_query_2.sql
@@ -38,3 +38,18 @@ WHERE MOD(ID, 2) = 0;
 --Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 -- Solution :
 SELECT COUNT (CITY) -(SELECT COUNT (DISTINCT CITY) FROM STATION) FROM STATION;
+
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+-- Solution:
+/*
+Enter your query here.
+*/
+(SELECT CITY, LENGTH(CITY)
+ FROM STATION
+ ORDER BY LENGTH(CITY), CITY
+ LIMIT 1)
+UNION ALL
+(SELECT CITY, LENGTH(CITY)
+ FROM STATION
+ ORDER BY LENGTH(CITY) DESC, CITY
+ LIMIT 1);
