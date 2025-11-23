9024344c7ddcc49b360a68e9da7690134f776c87
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-11-23 06:02:39 +0530

    group by , count

--- HR2_select_query_2.sql
+++ HR2_select_query_2.sql
@@ -53,3 +53,14 @@ UNION ALL
  FROM STATION
  ORDER BY LENGTH(CITY) DESC, CITY
  LIMIT 1);
+
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+--Solution :
+SELECT DISTINCT CITY 
+FROM STATION 
+WHERE CITY LIKE '%a'
+   OR CITY LIKE '%e'
+   OR CITY LIKE '%i'
+   OR CITY LIKE '%o'
+   OR CITY LIKE '%u';
+   
