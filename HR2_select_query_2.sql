17d917e413aa173d2b9e9cd0f41737f3b965c6f6
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-11-27 06:30:11 +0530

    not like

--- HR2_select_query_2.sql
+++ HR2_select_query_2.sql
@@ -101,3 +101,12 @@ or city like 'e%'
 or city like 'i%'
 or city like 'o%'
 or city like 'u%';
+
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+-- Solution :
+select distinct city from station 
+where city not like '%a'
+and city not like '%e'
+and city not like '%i'
+and city not like '%o'
+and city not like '%u';
