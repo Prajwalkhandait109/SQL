c944290282c1eddc68d4eb16d030a2c75eb8f31a
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-12-01 17:27:40 +0530

    order by right(colm_name,number)

--- HR2_select_query_2.sql
+++ HR2_select_query_2.sql
@@ -135,3 +135,8 @@ and city not like '%i'
 and city not like '%o'
 and city not like '%u';
 
+-- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+-- Solution :
+SELECT Name from STUDENTS
+where Marks > 75
+ORDER BY RIGHT (Name,3), ID ;
