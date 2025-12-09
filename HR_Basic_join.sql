c0f6904478ae532fc9409a4b9cbe091c60c874c6
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-12-09 22:07:18 +0530

    floor avg

--- HR_Basic_join.sql
+++ HR_Basic_join.sql
@@ -18,3 +18,16 @@ Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 select city.name from city
 inner join country on city.countrycode = country.code
 where continent = "Africa";
+
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+*/
+-- Solution :
+SELECT
+    country.continent,
+    FLOOR(AVG(city.population)) AS avg_population
+FROM country
+inner JOIN city
+    ON country.code = city.countrycode
+GROUP BY country.continent;
