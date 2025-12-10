7790615a1cee25856698693f65257ded47457c87
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-12-10 07:40:44 +0530

    join

--- HR_Basic_join.sql
+++ HR_Basic_join.sql
@@ -31,3 +31,25 @@ FROM country
 inner JOIN city
     ON country.code = city.countrycode
 GROUP BY country.continent;
+
+/*
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+Write a query to help Eve.
+*/
+-- Solution :
+SELECT 
+    CASE 
+        WHEN g.grade >= 8 THEN s.name 
+        ELSE 'NULL' 
+    END AS Name,
+    g.grade AS Grade,
+    s.marks AS Mark
+FROM students s
+JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
+ORDER BY 
+    g.grade DESC,
+    CASE 
+        WHEN g.grade >= 8 THEN s.name 
+    END ASC,
+    s.marks ASC;
