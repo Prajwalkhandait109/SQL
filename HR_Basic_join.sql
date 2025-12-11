c1b9155cb875986b6d76267d4d9dc37657bd124f
Prajwalkhandait109 <khandaitprajwal@gmail.com>
2025-12-11 10:32:35 +0530

    multiple join

--- HR_Basic_join.sql
+++ HR_Basic_join.sql
@@ -53,3 +53,31 @@ ORDER BY
         WHEN g.grade >= 8 THEN s.name 
     END ASC,
     s.marks ASC;
+
+
+/*
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+*/
+--Solution :
+/*
+Enter your query here.
+*/
+SELECT
+    h.hacker_id,
+    h.name
+FROM Hackers h
+JOIN Submissions s
+    ON h.hacker_id = s.hacker_id
+JOIN Challenges c
+    ON s.challenge_id = c.challenge_id
+JOIN Difficulty d
+    ON c.difficulty_level = d.difficulty_level
+WHERE s.score = d.score              -- full score for that challenge
+GROUP BY
+    h.hacker_id,
+    h.name
+HAVING
+    COUNT(DISTINCT s.challenge_id) > 1
+ORDER BY
+    COUNT(DISTINCT s.challenge_id) DESC,
+    h.hacker_id ASC;
